9b597d6afe92f516fdc617f8d4db35991acadb8a
nouriach <nouriach17@gmail.com>
2023-11-25 21:22:15 +0000

    Add Postcode to DTO
    When the `MakerPublishedDto` object is deserialized in the
    ProductService it requires a `Postcode` value. Before this wasn't
    being sent over as I was following the original tutorial.
    
    This means that in the `ProductService`, the database will save
    a `Maker` with an `id` and a `postcode` value. It won't know
    anything about the `FirstName` or `LastName`.

--- Domain/DTOs/MakerPublishedDto.cs
+++ Domain/DTOs/MakerPublishedDto.cs
@@ -3,6 +3,6 @@ namespace ProfileService.Domain.DTOs;
 public class MakerPublishedDto
 {
     public int Id { get; set; }
-    public string Name { get; set; }
+    public string Postcode { get; set; }
     public string Event { get; set; }
 }
